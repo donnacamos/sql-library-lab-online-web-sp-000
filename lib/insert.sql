a134780b7d5d3bb15b77c6018d2005aa643f0b68
Donna Amos <donnacamos.developer@gmail.com>
2019-06-07 01:31:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,7 +7,7 @@ INSERT INTO subgenres (id, name) VALUES (2, "space opera");
 INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"); INSERT INTO authors (id, name) VALUES (2, "Second Author");
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1);   
-(2, "A Clash of Kings", 1998, 1); 
+INSERT INTO books (id, title, year, series_id) VALUES (2, "A Clash of Kings", 1998, 1); 
 (3, "A Storm of Swords", 2000, 1); 
 (4, "First Book", 2002, 2);
 (5, "Second Book", 2003, 2); 
