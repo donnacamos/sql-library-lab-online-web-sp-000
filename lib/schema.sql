01a28b613359ae4665402d2a92ccd8f06e0a9b4e
Donna Amos <donnacamos.developer@gmail.com>
2019-06-07 01:13:10 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE characters ( id PRIMARY KEY INTEGER, name TEXT, motto TEXT, species TEXT, authors_id INTEGER, series_id INTEGER); 
 
-CREATE TABLE books ( id PRIMARY KEY INTEGER, title TEXT, ); 
+CREATE TABLE books ( id PRIMARY KEY INTEGER, title TEXT, year INTEGER, series_id INTEGER);  
 
 CREATE TABLE series (); 
 
