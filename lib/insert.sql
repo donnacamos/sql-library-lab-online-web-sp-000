e30b9d6c0a52956494e16d6457a9c8ef2979aeb7
Donna Amos <donnacamos.developer@gmail.com>
2019-06-07 01:31:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,7 +8,7 @@ INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"); INSERT INTO au
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1);   
 INSERT INTO books (id, title, year, series_id) VALUES (2, "A Clash of Kings", 1998, 1); 
-(3, "A Storm of Swords", 2000, 1); 
+INSERT INTO books (id, title, year, series_id) VALUES (3, "A Storm of Swords", 2000, 1); 
 (4, "First Book", 2002, 2);
 (5, "Second Book", 2003, 2); 
 (6, "Third Book", 2005, 2);
