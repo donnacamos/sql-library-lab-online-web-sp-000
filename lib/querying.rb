1a84fbac648175da8444adc5e7b3efb3892432aa
Donna Amos <donnacamos.developer@gmail.com>
2019-06-07 01:57:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) AS num_species FROM characters
      GROUP BY species
-     ORDER BY num_species 
+     ORDER BY num_species DESC 
+     
   "
 end
 
