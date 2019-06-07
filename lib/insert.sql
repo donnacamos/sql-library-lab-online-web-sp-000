814b0b6846964ecb5b2d61a3b9c961525fb73661
Donna Amos <donnacamos.developer@gmail.com>
2019-06-07 01:37:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -38,5 +38,5 @@ INSERT INTO character_books (id, book_id, character_id) VALUES (11, 5, 6);
 INSERT INTO character_books (id, book_id, character_id) VALUES (12, 6, 6); 
 INSERT INTO character_books (id, book_id, character_id) VALUES (13, 4, 7); 
 INSERT INTO character_books (id, book_id, character_id) VALUES (14, 5, 7);
-(15, 6, 7);
+INSERT INTO character_books (id, book_id, character_id) VALUES (15, 6, 7);
 (16, 4, 8);
